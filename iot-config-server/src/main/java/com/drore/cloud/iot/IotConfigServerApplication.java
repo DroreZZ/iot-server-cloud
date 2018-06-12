ca274b8c177e42c7f34416aefabe5efe30799090
zhangz <zhangz@drore.com>
2018-06-12 13:28:44 +0800

    客户端注册

--- iot-config-server/src/main/java/com/drore/cloud/iot/IotConfigServerApplication.java
+++ iot-config-server/src/main/java/com/drore/cloud/iot/IotConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.drore.cloud.iot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class IotConfigServerApplication {
 
 	public static void main(String[] args) {
